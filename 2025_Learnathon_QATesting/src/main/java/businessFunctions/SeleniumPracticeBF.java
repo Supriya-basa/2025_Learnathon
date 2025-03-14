c6d7886a19f3d041b0cde2a3494d99fbb9ba41c0
Supriya Basa <supriyabasa@gmail.com>
2025-03-15 05:14:29 +0530

    Commiting to main branch

--- 2025_Learnathon_QATesting/src/main/java/businessFunctions/SeleniumPracticeBF.java
+++ 2025_Learnathon_QATesting/src/main/java/businessFunctions/SeleniumPracticeBF.java
@@ -1,11 +1,22 @@
 package businessFunctions;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class SeleniumPracticeBF {
 
 
     public void openFormPage() {
         // Code to open the form page
         String url="https://www.seleniumeasy.com/test/basic-first-form-demo.html";
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
+
     }
 
 
