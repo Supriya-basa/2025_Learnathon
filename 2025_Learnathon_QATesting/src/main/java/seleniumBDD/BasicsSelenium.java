f829374a0a0cec52b2f9abb7ce9aea3c058972b6
Supriya Basa <supriyabasa@gmail.com>
2025-03-15 05:10:18 +0530

    Commit through Terminal

--- 2025_Learnathon_QATesting/src/main/java/seleniumBDD/BasicsSelenium.java
+++ 2025_Learnathon_QATesting/src/main/java/seleniumBDD/BasicsSelenium.java
@@ -9,7 +9,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -52,6 +51,9 @@ public class BasicsSelenium {
         }
     }
 
+
+
+
     @AfterClass
     public void quitDriver() {
         if (driver != null) {
@@ -59,4 +61,21 @@ public class BasicsSelenium {
             System.out.println("Driver quit successfully");
         }
     }
+
+    public static void interviewPreparation()
+    {
+        WebDriver driver = new ChromeDriver();
+        WebElement element = driver.findElement(By.xpath("//input[text()='Colony State']"));
+        boolean isDisplayed= element.isDisplayed();
+        if(isDisplayed)
+        {
+            System.out.println("Element is displayed");
+        }
+        else
+        {
+            System.out.println("Element is not displayed");
+        }
+
+    }
+
 }
